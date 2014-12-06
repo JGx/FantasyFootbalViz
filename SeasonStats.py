376057aab958fd3e6409a8ea3a90bc3b752aaae5
ldahill <luke.dahill@gmail.com>
2014-12-06 15:40:38 -0500

    Full data retrieval 2009 - 2013

--- SeasonStats.py
+++ SeasonStats.py
@@ -18,7 +18,6 @@ class PlayerStats:
 		if self.stats == {} or plyr not in self.stats:
 			self.stats[plyr]={}
 		self.stats[plyr][season] = self.getSeasonStats(plyr, stat, season)
-			
 
 	def write(self, filename):
 		jsonf = open(filename,'w') 
@@ -27,21 +26,26 @@ class PlayerStats:
 
 	def getSeasonStats(self, plyr, stat, season):   
 		data = []
-		for x in range(1, 17):
-			print "LOOP: "+ str(x)
+		for x in range(1, 18):
 			game = nflgame.games(season,week=x)
 			players = nflgame.combine_game_stats(game)
 			player = players.name(plyr)
 			if player is None:
-				print player
-				data.append({'week':str(x),stat:"N/A"})
+				data.append({'week':str(x),'active':'false'})
 			else:
 				playerStats = player.stats
-				data.append({'week':str(x),stat:str(playerStats[stat])})
+				week_obj = {'week':str(x)}
+				if(stat == 'all'):
+					for x in playerStats:
+						week_obj[x] = playerStats[x]
+					week_obj['active'] = 'true'
+					data.append(week_obj)
+				else:
+					data.append({'week':str(x),stat:str(playerStats[stat]), 'active':'true'})
 		return data
 
 
 plyrstats = PlayerStats()
-plyrstats.addSeasons('P.Manning','passing_yds', 2009, 2013)
+plyrstats.addSeasons('P.Manning','all', 2009, 2013)
 plyrstats.write("ManningPassing.json")
 
