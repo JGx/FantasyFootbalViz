c9e903bda77e376fadf554d1a157e80f34107f67
msilverblatt <msilverblatt@gmail.com>
2014-12-09 20:39:39 -0500

    player lookup works:

--- manning.js
+++ manning.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	//initManningGraph();
-  testNewGraph();
+ // testNewGraph();
 })
 
 
--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $.get("playersHash.json",function(data){ player_lookup = data; });
-	d3.json("2012_passing.json",function(data){
+	/**
+    d3.json("2012_passing.json",function(data){
 		data_graphic({
 			chart_type : "bar",
     		title: "Football",
@@ -14,6 +15,7 @@ $(document).ready(function(){
 
 
 		})});
+**/
     $(function(){
    // player_list = ["Peyton Manning", "Eli Manning", "Archie Manning"]
     $.get("player_list.txt", function(data){
@@ -65,5 +67,98 @@ $("#go").click(function(){
       })*/
         })
         }
-        initGraph();
+    function newGraph(q){
+  d3.json(q,function(player_data){
+    p_name = $("#player_input").val();
+    p_code = Object.keys(player_data)[0]
+    console.log(p_code);
+    var series =[];
+    var max_y = 0;
+    for(var week_data in player_data[p_code][2009]){
+      var x_val = parseInt(player_data[p_code][2009][week_data]['week']);
+      var y_val = player_data[p_code][2009][week_data]['active'] == 'true' ? player_data[p_code][2009][week_data]['passing_yds'] : null;
+      if(y_val > max_y){
+        max_y = y_val;
+      }
+      series.push({x:x_val, y:y_val})
+    }
+
+    console.log(series);
+
+    var data = d3.range(40).map(function(i) {
+      return {x: i / 39, y: i % 5 ? (Math.sin(i / 3) + 2) / 4 : null};
+    });
+    console.log(data);
+    data = series;
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = $('.container').width() - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+    var x = d3.scale.linear()
+    .domain([1,17])
+    .range([0, width]);
+
+    var y = d3.scale.linear()
+    .domain([0,Math.ceil(max_y/50)*50])
+    .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+    var line = d3.svg.line()
+    .defined(function(d) { return d.y != null; })
+    .x(function(d) { return x(d.x); })
+    .y(function(d) { return y(d.y); });
+
+    var area = d3.svg.area()
+    .defined(line.defined())
+    .x(line.x())
+    .y1(line.y())
+    .y0(y(0));
+
+    var svg = d3.select("#second").append("svg")
+    .datum(data)
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    svg.append("path")
+    .attr("class", "area")
+    .attr("d", area);
+
+    svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+    svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
+
+    svg.append("path")
+    .attr("class", "line")
+    .attr("d", line);
+
+    svg.selectAll(".dot")
+    .data(data.filter(function(d) { return d.y; }))
+    .enter().append("circle")
+    .attr("class", "dot")
+    .attr("cx", line.x())
+    .attr("cy", line.y())
+    .attr("r", 3.5);
+  })
+}
+
+
+
+        //initGraph();
+        $("#second").empty();
+        newGraph(query);
     });
