b9283573c7afec8fcf50d95545e3f467b147bb05
ldahill <luke.dahill@gmail.com>
2014-12-10 17:05:20 -0500

    Changing Player 2

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
+var graphCreated = false;
+
+
+
+
 $(document).ready(function(){
-    $.get("playersHash.json",function(data){ player_lookup = data; });
+  $.get("playersHash.json",function(data){ player_lookup = data; });
   /**
     d3.json("2012_passing.json",function(data){
     data_graphic({
@@ -16,50 +21,45 @@ $(document).ready(function(){
 
     })});
 **/
-    $(function(){
+$(function(){
    // player_list = ["Peyton Manning", "Eli Manning", "Archie Manning"]
-    $.get("list_with_pos.txt", function(data){
-        player_list = data.split(",");
-        jQuery( "#player_input_one" ).autocomplete({
-        source: player_list
-        });
-        jQuery("#player_input_two").autocomplete({
-            source: player_list
-        });
-        })
+   $.get("list_with_pos.txt", function(data){
+    player_list = data.split(",");
+    jQuery( "#player_input_one" ).autocomplete({
+      source: player_list
     });
+    jQuery("#player_input_two").autocomplete({
+      source: player_list
+    });
+  })
+ });
 
 });
 
+function getDataPoints(player_data, p_code, years, data){
+  var max_y = 0;
+  for(var week_data in player_data[p_code][2009]){
+    var x_val = parseInt(player_data[p_code][2009][week_data]['week']);
+    var y_val = player_data[p_code][2009][week_data]['active'] == 'true' ? player_data[p_code][2009][week_data]['passing_yds'] : null;
+    if(y_val > max_y){
+      max_y = y_val;
+    }
+    data.push({x:x_val, y:y_val})
+  }
+  return max_y;
+}
 
-    function newGraph(q, num){
+function newGraph(q, num){
   d3.json(q,function(player_data){
     if (num == 1) p_name = $("#player_input_one").val();
     else p_name = $("#player_input_two").val();
     var years = $("#slider").slider("option","values");
     console.log("YEARS IS",years);
     console.log(p_name);
+
     p_code = Object.keys(player_data)[0]
-    console.log(p_code);
-    var series =[];
-    var max_y = 0;
-    for(var week_data in player_data[p_code][2009]){
-      var x_val = parseInt(player_data[p_code][2009][week_data]['week']);
-      var y_val = player_data[p_code][2009][week_data]['active'] == 'true' ? player_data[p_code][2009][week_data]['passing_yds'] : null;
-      if(y_val > max_y){
-        max_y = y_val;
-      }
-      series.push({x:x_val, y:y_val})
-    }
-
-    console.log(series);
-
-    var data = d3.range(40).map(function(i) {
-      return {x: i / 39, y: i % 5 ? (Math.sin(i / 3) + 2) / 4 : null};
-    });
-    console.log(data);
-    data = series;
-
+    var data = [];
+    var max_y  = getDataPoints(player_data, p_code, 2009, data);
 
     var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = $('.container').width() - margin.left - margin.right,
@@ -91,14 +91,14 @@ $(document).ready(function(){
     .x(line.x())
     .y1(line.y())
     .y0(y(0));
-    if (num == 1) target = "#first"
-        else target = "#second"
+    target = "#first"
+
     var svg = d3.select(target).append("svg")
-    .datum(data)
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      .datum(data)
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     // svg.append("path")
     // .attr("class", "area")
@@ -113,152 +113,166 @@ $(document).ready(function(){
     .attr("class", "y axis")
     .call(yAxis);
 
+
     svg.append("path")
     .attr("class", "line")
     .attr("d", line);
 
-    svg.selectAll(".dot")
+    svg.selectAll(".one .dot")
     .data(data.filter(function(d) { return d.y; }))
     .enter().append("circle")
-    .attr("class", "dot")
+    .attr("class", "one dot")
     .attr("cx", line.x())
     .attr("cy", line.y())
     .attr("r", 3.5)
     .on("mouseover",function(){
-                    console.log("mouse over point");
-                    console.log(d3.mouse(this)[0]);
-                    var x0 = Math.round(x.invert(d3.mouse(this)[0]));
-                    console.log("x0 is",x0);
-                    pcode = Object.keys(player_data)[0],
-                    i = player_data[pcode][2009][x0-1]['passing_yds'];
-                    console.log("passing yards is",i);
-                  });
+      console.log("mouse over point");
+      console.log(d3.mouse(this)[0]);
+      var x0 = Math.round(x.invert(d3.mouse(this)[0]));
+      console.log("x0 is",x0);
+      pcode = Object.keys(player_data)[0],
+      i = player_data[pcode][2009][x0-1]['passing_yds'];
+      console.log("passing yards is",i);
+    });
+
+    graphCreated = true;
   })
 }
 
 
-function add2Graph(q){
+function add2Graph(q, num){
   d3.json(q,function(player_data){
+  
 
-      p_name = $("#player_input_two").val();
-
-      p_code = Object.keys(player_data)[0]
-      console.log(p_code);
-      var series =[];
-      var max_y = 0;
-      for(var week_data in player_data[p_code][2009]){
-        var x_val = parseInt(player_data[p_code][2009][week_data]['week']);
-        var y_val = player_data[p_code][2009][week_data]['active'] == 'true' ? player_data[p_code][2009][week_data]['passing_yds'] : null;
-        if(y_val > max_y){
-          max_y = y_val;
-        }
-        series.push({x:x_val, y:y_val})
-      }
+    p_code = Object.keys(player_data)[0]
+    console.log(p_code);
+    var series = [];
+    var max_y  = getDataPoints(player_data, p_code, 2009, series);
 
     var margin = {top: 20, right: 20, bottom: 30, left: 40},
-      width = $('.container').width() - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+    width = $('.container').width() - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
 
     var x = d3.scale.linear()
-      .domain([1,17])
-      .range([0, width]);
+    .domain([1,17])
+    .range([0, width]);
 
     var y = d3.scale.linear()
     .domain([0,Math.ceil(max_y/50)*50])
     .range([height, 0]);
 
-      var line2 = d3.svg.line()
-        .defined(function(d) { return d.y != null; })
-        .x(function(d) { return x(d.x); })
-        .y(function(d) { return y(d.y); });
+    var line2 = d3.svg.line()
+    .defined(function(d) { return d.y != null; })
+    .x(function(d) { return x(d.x); })
+    .y(function(d) { return y(d.y); });
 
-      var area2 = d3.svg.area()
-      .defined(line2.defined())
-      .x(line2.x())
-      .y1(line2.y())
-      .y0(y(0));
+    var area2 = d3.svg.area()
+    .defined(line2.defined())
+    .x(line2.x())
+    .y1(line2.y())
+    .y0(y(0));
 
 
-      var svg = d3.select("#first > svg > g")
+    var svg = d3.select("#first > svg > g")
+
+    if(num == 1){
+      console.log("yo");
+      $("#first > svg > g .line").remove();
+      $("#first > svg > g .one.dot").remove();
       svg.append("path")
-        .attr("class", "line2")
-        .attr("d", line2(series));
+      .attr("class", "line")
+      .attr("d", line2(series));
 
-      //   svg.append("path")
-      // .attr("class", "area2")
-      // .attr("d", area2);
+        svg.selectAll(".one .dot")
+        .data(series.filter(function(d) { return d.y; }))
+        .enter().append("circle")
+        .attr("class", "one dot")
+        .attr("cx", line2.x())
+        .attr("cy", line2.y())
+        .attr("r", 3.5);
+      
+    }
+    else if(num == 2){
+      $("#first > svg > g .line2").remove();
+      $("#first > svg > g .two.dot").remove();
+      svg.append("path")
+      .attr("class", "line2")
+      .attr("d", line2(series));
 
-      svg.selectAll(".two .dot")
+        svg.selectAll(".two .dot")
         .data(series.filter(function(d) { return d.y; }))
         .enter().append("circle")
         .attr("class", "two dot")
         .attr("cx", line2.x())
         .attr("cy", line2.y())
         .attr("r", 3.5);
-    })
+      
+    }
+  })
 }
 
 
 $("#go_two").click(function(){
-    console.log("here");
-    if ($("#pos").val() == "all") { query = "player_data/" + $("#player_input_two").val().slice(0,-3).replace(" ","").split(" ").join("_") + ".json"; }
-        else { query = "player_data/" + $("#player_input_two").val().replace(" ","").split(" ").join("_") + ".json"; }
-   // console.log("query");
-   // $.get(query, function(data) {
-     //   current_data = data;
-       // console.log(data)
-        //initGraph();
-        $("#second").empty();
-        add2Graph(query);
-       // newGraph(query,2);
-    });
+  console.log("here");
+  if ($("#pos").val() == "all") { query = "player_data/" + $("#player_input_two").val().slice(0,-3).replace(" ","").split(" ").join("_") + ".json"; }
+  else { query = "player_data/" + $("#player_input_two").val().replace(" ","").split(" ").join("_") + ".json"; }
+
+  $("#second").empty();
+  if(!graphCreated){
+    newGraph(query,2);
+  }
+  else{
+    add2Graph(query, 2);
+  }
+});
 
 
 
 current_data = null;
 $("#go_one").click(function(){
-    console.log("here");
-    if ($("#pos").val() == "all") { query = "player_data/" + $("#player_input_one").val().slice(0,-3).replace(" ","").split(" ").join("_") + ".json"; }
-        else { query = "player_data/" + $("#player_input_one").val().replace(" ","").split(" ").join("_") + ".json"; }
-    
-   // console.log("query");
-   // $.get(query, function(data) {
-     //   current_data = data;
-       // console.log(data)
-        //initGraph();
-        $("#first").empty();
-        newGraph(query,1);
-    });
+  console.log("here");
+  if ($("#pos").val() == "all") { query = "player_data/" + $("#player_input_one").val().slice(0,-3).replace(" ","").split(" ").join("_") + ".json"; }
+  else { query = "player_data/" + $("#player_input_one").val().replace(" ","").split(" ").join("_") + ".json"; }
+
+
+  $("#first").empty();
+  if(!graphCreated){
+    newGraph(query,1);
+  }
+  else{
+    add2Graph(query, 1);
+  }
+});
 
 
 $("#pos").change(function(){
-    target = "";
-    switch($("#pos").val()){
-        case "all":
-            target = "list_with_pos.txt";
-            break;
-        case "qb":
-            target = "qbs.txt";
-            break;
-        case "rb":
-            target = "rbs.txt";
-            break;
-        case "wr":
-            target = "wrs.txt";
-            break
-        case "te":
-            target = "tes.txt";
-            break;
+  target = "";
+  switch($("#pos").val()){
+    case "all":
+    target = "list_with_pos.txt";
+    break;
+    case "qb":
+    target = "qbs.txt";
+    break;
+    case "rb":
+    target = "rbs.txt";
+    break;
+    case "wr":
+    target = "wrs.txt";
+    break
+    case "te":
+    target = "tes.txt";
+    break;
 
-    }
-    $.get(target, function(data){
-        player_list = data.split(",");
-        jQuery( "#player_input_one" ).autocomplete({
-        source: player_list
-        });
-        jQuery("#player_input_two").autocomplete({
-            source: player_list
-        });
+  }
+  $.get(target, function(data){
+    player_list = data.split(",");
+    jQuery( "#player_input_one" ).autocomplete({
+      source: player_list
     });
+    jQuery("#player_input_two").autocomplete({
+      source: player_list
+    });
+  });
 });
